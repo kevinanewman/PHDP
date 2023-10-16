783d8f07fd3c7d32aa4145abe416211e7338983f
kevinnewman <newman.kevin@epa.gov>
2023-10-16 16:25:47 -0400

    first working cut at creating time-aligned data and calculated values    * has some hard-coded values, etc, but should be working correctly

--- phdp/phdp.py
+++ phdp/phdp.py
@@ -10,6 +10,8 @@ PHDP top level code
 
 import sys, os
 
+import pandas as pd
+
 path = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, os.path.join(path, '..'))  # picks up omega_model sub-packages
 
@@ -43,21 +45,23 @@ def init_phdp(runtime_options):
 
 def get_unitized_columns(filename, sheet_name=None, ignore_units=['Text']):
     """
+    Combine column labels and units row into a single combined string to identify the column.
 
     Args:
-        filename:
-        sheet_name:
-        ignore_units:
+        filename (str): name of the file to read
+        sheet_name (str): for reading a particular Excel sheet, or ``None`` for CSV files
+        ignore_units (list of str): unit values to ignore, default ``Text``
 
     Returns:
+        List of combined column headers and units as in ``['ColumnName_units', ...]``
 
     """
     if sheet_name:
         columns = pd.read_excel(filename, header=None, nrows=1, sheet_name=sheet_name)
         units = pd.read_excel(filename, header=None, skiprows=1, nrows=1, sheet_name=sheet_name)
     else:
-        columns = pd.read_csv(filename, header=None, nrows=1, encoding_errors='ignore')
-        units = pd.read_csv(filename, header=None, skiprows=1, nrows=1, encoding_errors='ignore')
+        columns = pd.read_csv(filename, header=None, nrows=1,  encoding='cp1252', encoding_errors='strict')
+        units = pd.read_csv(filename, header=None, skiprows=1, nrows=1, encoding='cp1252', encoding_errors='strict')
 
     unitized_columns = []
 
@@ -86,6 +90,7 @@ def load_data():
     phdp_globals.test_data['horiba_data'] = \
         pd.read_excel(phdp_globals.options.horiba_file, names=unitized_columns, header=1, skiprows=0,
                       sheet_name='ContinuousData1')
+
     # read in the rest of the files just in case
     os.chdir(file_io.get_filepath(phdp_globals.options.horiba_file))
     input_files = sorted([f for f in os.listdir() if f.endswith('.csv')])
@@ -95,7 +100,8 @@ def load_data():
             phdp_log.logwrite('reading %s...' % input_file)
             unitized_columns = get_unitized_columns(input_file)
             phdp_globals.test_data[file_name] = \
-                pd.read_csv(input_file, names=unitized_columns, encoding_errors='ignore', header=1, skiprows=0)
+                pd.read_csv(input_file, names=unitized_columns, encoding='cp1252', encoding_errors='strict',
+                            header=1, skiprows=0)
 
 
 def run_phdp(runtime_options):
@@ -124,9 +130,67 @@ def run_phdp(runtime_options):
 
             phdp_log.logwrite('\nProcessing test %s (%s) from %s...\n' % (test_num, test_type, test_site))
 
+            # load all raw data, even if not all required for now
             load_data()
 
+            # pull in raw data and time align as necessary
+            from test_sites import test_sites
 
+            ContinuousLoggerPeriod_s = phdp_globals.test_data['TestParameters']['ContinuousLoggerPeriod_s'].item()
+            time_aligned_data = pd.DataFrame(index=phdp_globals.test_data['ContinuousData'].index)
+
+            for source in test_sites[test_site]['signals_and_delays'].keys():
+                print(source)
+                for signal in test_sites[test_site]['signals_and_delays'][source]:
+                    print(signal)
+                    delay_s = test_sites[test_site]['signals_and_delays'][source][signal]
+                    delay_samples = round(delay_s / ContinuousLoggerPeriod_s)
+                    time_aligned_data = pd.concat([time_aligned_data,
+                                                   pd.DataFrame({signal: phdp_globals.test_data[source][signal]
+                                                                .iloc[delay_samples:].values})], axis=1)
+            time_aligned_data = \
+                time_aligned_data[time_aligned_data['EmissionsCycleNumber_Integer'] == 1].reset_index(drop=True)
+
+            # add vehicle moving flag
+            test_cycle_definition = phdp_globals.test_data['CycleDefinition'][phdp_globals.test_data['CycleDefinition']
+                                                                              ['EmissionsCycleNumber_Integer'] == 1]
+            vehicle_moving_int = test_cycle_definition['VehicleMoving_Logical']
+            time_aligned_data = pd.concat([time_aligned_data,
+                                           pd.DataFrame({'VehicleMoving_Logical': vehicle_moving_int.values})], axis=1)
+
+            # add calculated values
+            time_aligned_data['BagFillFlow_Avg_m³/s'] = time_aligned_data['BagFillFlow_Avg_l/min'] / 60000
+            time_aligned_data['CVSFlow_mol/s'] = \
+                (time_aligned_data['CVSFlow_Avg_m³/s'] + time_aligned_data['BagFillFlow_Avg_m³/s'] +
+                 phdp_globals.test_data['TestParameters']['DiluteSampleVolumeFlow_m³/s'].item()) / 0.024055
+            time_aligned_data['Tsat_K'] = time_aligned_data['CVSDilAirTemp_Avg_°C'] + 273.15
+
+            AF3 = time_aligned_data['Tsat_K'].values
+            # 1065.645-1:
+            time_aligned_data['pH2Odilsat_kPa'] = 10 ** ((10.79574 * (1 - (273.16 / AF3))) -
+                                                         (5.028 * np.log10(AF3 / 273.16)) +
+                                                         (0.000150475 * (1 - (10 ** (-8.2969 * ((AF3 / 273.16) - 1)))))
+                                                         + (0.00042873 * (10 ** (4.76955 * (1 - (273.16 / AF3))) - 1))
+                                                         - 0.2138602)
+            # 1065.645-4:
+            time_aligned_data['xH2Odil_mol/vol'] = \
+                time_aligned_data['CVSDilAirRH_Avg_%'] / 100 * time_aligned_data['pH2Odilsat_kPa'] / \
+                time_aligned_data['pCellAmbient_kPa']
+
+            time_aligned_data['pH2Odilscal_Pa'] = time_aligned_data['pH2Odilsat_kPa'] * \
+                                                  time_aligned_data['CVSDilAirRH_Avg_%'] / 100 * 1000
+            # 1065.645-5
+            AI3 = time_aligned_data['pH2Odilscal_Pa']
+            time_aligned_data['Tdewdil_K'] = (207.98233 - 20.156028 * np.log(AI3) + 0.46778925 *
+                                              (np.log(AI3)) ** 2 - 9.2288067 * 10 ** (-6) * (np.log(AI3)) ** 3) / \
+                                             (1 - 0.13319669 * np.log(AI3) +
+                                              5.6577518 * 10 ** (-3) * (np.log(AI3)) ** 2
+                                              - 7.5172865 * 10 ** (-5) * (np.log(AI3)) ** 3)
+
+            time_aligned_data['Tdewdil_°C'] = time_aligned_data['Tdewdil_K'] - 273.15
+            time_aligned_data['CVSDilAirDPTemp_°C'] = time_aligned_data['Tdewdil_°C']
+
+            time_aligned_data.to_csv('tad.csv', index=False)
 
     except:
         phdp_log.logwrite("\n#RUNTIME FAIL\n%s\n" % traceback.format_exc())
--- phdp/test_sites.py
+++ phdp/test_sites.py
@@ -3,44 +3,45 @@
 
 """
 
+from phdp import *
+
 test_sites = dict()
 
 test_sites['HD02'] = {
     'signals_and_delays':
-        {'horiba_data':
+        {
+            'ContinuousData':
             {
-                'Running_Logical': 0,
                 'ModeNumber_Integer': 0,
-                'CycleNumber_Integer': 0,
+                'EmissionsCycleNumber_Integer': 0,
                 'pCellAmbient_kPa': 0,
                 'spDyno_rev/min': 0,
                 'tqShaft_Nm': 0,
-                'qmIntakeAir_kg/h': 0,
+
+                'qmIntakeAir_Avg_kg/h': phdp_globals.test_data['TestParameters']['AirFlowDelay_s'].item(),
                 'qmFuel_g/h': phdp_globals.test_data['TestParameters']['FuelFlowDelay_s'].item(),
-                'DEFMassFlowRate_g/h': 0,
+                'DEFMassFlowRate_Avg_g/h': 0,
                 'tIntakeAir_°C': 0,
-                'IntakeAirPress_kPa': 0,
+                'IntakeAirPress_Avg_kPa': 0,
                 'tCellDewPt_°C': 0,
-                'CVSDilExhTemp_°C': 0,
-                'CVSDilAirRH_%': 0,
-                'CVSDilAirTemp_°C': 0,
-                'conRawCO2_%mol': 7.1,
-                'conRawHCO_µmol/mol': 7.5,
-                'conRawNOX_µmol/mol': 5.5,
-                'conRawTHC_µmol/mol': 4.1,
-                'conRawCH4cutter_µmol/mol': 4.3,
-                'conRawO2_%mol': 0,  # or 7.6?
-                'conRawNH3_µmol/mol': 0,  # or 5.0?
-                'conCO2_%mol': 9.3,
-                'conLCO_µmol/mol': 10.3,
-                'conNOX_µmol/mol': 6.5,
-                'conN2O_µmol/mol': 9.0,
-                'conTHC_µmol/mol': 6.3,
-                'conCH4cutter_µmol/mol': 7.5,
-                'CVSFlow_m³/s': phdp_globals.test_data['TestParameters']['AirFlowDelay_s'].item(),
+
+                'CVSDilAirRH_Avg_%': 0,
+                'CVSDilAirTemp_Avg_°C': 0,
+                'conRawCO2_Avg_%vol': phdp_globals.test_data['EmsComponents'].loc[phdp_globals.test_data['EmsComponents']['ParameterName'] == 'RawCO2_System']['SampleDelay_s'].item(),
+                'conRawHCO_Avg_ppm': phdp_globals.test_data['EmsComponents'].loc[phdp_globals.test_data['EmsComponents']['ParameterName'] == 'RawCO_System']['SampleDelay_s'].item(),
+                'conRawNOX_Avg_ppm': phdp_globals.test_data['EmsComponents'].loc[phdp_globals.test_data['EmsComponents']['ParameterName'] == 'RawNOx_System']['SampleDelay_s'].item(),
+                'conRawTHC_Avg_ppmC': phdp_globals.test_data['EmsComponents'].loc[phdp_globals.test_data['EmsComponents']['ParameterName'] == 'RawTHC_System']['SampleDelay_s'].item(),
+                'conRawCH4cutter_Avg_ppmC': phdp_globals.test_data['EmsComponents'].loc[phdp_globals.test_data['EmsComponents']['ParameterName'] == 'RawCH4_System']['SampleDelay_s'].item(),
+                'conRawO2_Avg_%vol': phdp_globals.test_data['EmsComponents'].loc[phdp_globals.test_data['EmsComponents']['ParameterName'] == 'RawO2_System']['SampleDelay_s'].item(),
+                'conRawNH3_Avg_ppm': phdp_globals.test_data['EmsComponents'].loc[phdp_globals.test_data['EmsComponents']['ParameterName'] == 'RawNH3_System']['SampleDelay_s'].item(),
+                'conCO2_Avg_%vol': phdp_globals.test_data['EmsComponents'].loc[phdp_globals.test_data['EmsComponents']['ParameterName'] == 'DilCO2_System']['SampleDelay_s'].item(),
+                'conLCO_Avg_ppm': phdp_globals.test_data['EmsComponents'].loc[phdp_globals.test_data['EmsComponents']['ParameterName'] == 'DilCO_System']['SampleDelay_s'].item(),
+                'conNOX_Avg_ppm': phdp_globals.test_data['EmsComponents'].loc[phdp_globals.test_data['EmsComponents']['ParameterName'] == 'DilNOx_System']['SampleDelay_s'].item(),
+                'conN2O_Avg_ppm': phdp_globals.test_data['EmsComponents'].loc[phdp_globals.test_data['EmsComponents']['ParameterName'] == 'DilN2O_System']['SampleDelay_s'].item(),
+                'conTHC_Avg_ppmC': phdp_globals.test_data['EmsComponents'].loc[phdp_globals.test_data['EmsComponents']['ParameterName'] == 'DilTHC_System']['SampleDelay_s'].item(),
+                'conCH4cutter_Avg_ppmC': phdp_globals.test_data['EmsComponents'].loc[phdp_globals.test_data['EmsComponents']['ParameterName'] == 'DilCH4_System']['SampleDelay_s'].item(),
+                'CVSFlow_Avg_m³/s': 0,
+                'BagFillFlow_Avg_l/min': 0,
             },
-            'CycleDefinition': {
-                'VehicleMoving_Logical': 0,
-            }
-        }
-}
+        },  # signals_and_delays
+}  # HD02
