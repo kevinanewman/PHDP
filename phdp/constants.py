aef4724002e884b34e4dd9186e02748d526a53ed
RSun <sun.ruonan@epa.gov>
2023-10-18 11:53:41 -0400

    started work on physical constants

---
+++ phdp/constants.py
@@ -0,0 +1,19 @@
+"""
+    Physical constants
+
+"""
+
+constants = {
+    'Mair_g/mol':	28.96559,
+    MC	g/mol	12.0107
+    MCO2	g/mol	44.0095
+    MCO	g/mol	28.0101
+    MTHC	g/mol	13.8754
+    MCH4	g/mol	16.0425
+    MNMHC	g/mol	13.8754
+    MNOx	g/mol	46.0055
+    MN2O	g/mol	44.0128
+    MH	g/mol	1.01
+    MCH4N2O	g/mol	60.05526
+    wCH4N2O	Mass Fraction of urea in DEF	0.325
+}
