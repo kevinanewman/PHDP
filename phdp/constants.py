778b5301863e4a393e6bbde172e2096bff2d4436
kevinnewman <newman.kevin@epa.gov>
2023-10-18 16:49:12 -0400

    updated constants with dynamic fuel values and calculated DEF values some prior entries were rounded, now they are not

--- phdp/constants.py
+++ phdp/constants.py
@@ -3,6 +3,8 @@
 
 """
 
+from phdp import *
+
 constants = {
     'KH2O-gas': 3.50,
     'Mair_g/mol': 28.96559,
@@ -12,18 +14,34 @@ constants = {
     'MS_g/mol': 32.0655,
     'MN_g/mol': 14.0067,
     'MCO_g/mol': 28.0101,
-    'MTHC_g/mol': 13.8754,
+    'MTHC_g/mol': 13.87539,
     'MCH4_g/mol': 16.0425,
-    'MNMHC_g/mol': 13.8754,
+    'MNMHC_g/mol': 13.87539,
     'MNOx_g/mol': 46.0055,
     'MN2O_g/mol': 44.0128,
-    'MH_g/mol': 1.01,
+    'MH_g/mol': 1.00794,
     'MCH4N2O_g/mol': 60.05526,
     'wCH4N2O_Mass Fraction of urea in DEF': 0.325,
-    'wcDef': 0.065017214,
-    'whDef': 0.097394165,
-    'woDef': 0.685944401,
-    'wnDef': 0.15164422,
     'EmfuelCref	MJ/kg': 49.3112,
     'wCref': 0.874,
+    'MH2O_g/mol': 18.01528,
 }
+
+
+def update_constants():
+    # DEF constants
+    constants['Mdef'] = constants['MC_g/mol'] + constants['MH_g/mol'] * 17.85 + constants['MO_g/mol'] * 7.92 + \
+                    constants['MN_g/mol'] * 2
+
+    constants['wcDef'] = constants['MC_g/mol'] / constants['Mdef']
+    constants['whDef'] = constants['MH_g/mol'] * 17.85 / constants['Mdef']
+    constants['woDef'] = constants['MO_g/mol'] * 7.92 / constants['Mdef']
+    constants['wnDef'] = constants['MN_g/mol'] * 2 / constants['Mdef']
+
+    # Test Fuel constants
+    constants['FuelHTCRAT_ratio'] = phdp_globals.test_data['Header']['FuelHTCRAT_ratio'].item()
+    constants['Mfuel_g/mol'] = constants['MC_g/mol'] + constants['FuelHTCRAT_ratio'] * constants['MH_g/mol']
+    constants['wcFuel'] = constants['MC_g/mol'] / constants['Mfuel_g/mol']
+    constants['whFuel'] = constants['FuelHTCRAT_ratio'] * constants['MH_g/mol'] / constants['Mfuel_g/mol']
+
+    print_dict(constants)
