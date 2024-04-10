6cb1379cf9e08e1586157be1c68a52dc973f095e
kevinnewman <newman.kevin@epa.gov>
2024-04-10 11:01:06 -0400

    cleanup

--- phdp/phdp.py
+++ phdp/phdp.py
@@ -237,10 +237,6 @@ def pre_chemical_balance_calculations(time_aligned_data, calc_mode, test_type):
 
             time_aligned_data['CVSDilAirTemp_Avg_°C'] = time_aligned_data_avg['CVSDilAirTemp_Avg_°C']
             time_aligned_data['CVSDilAirDPTemp_Avg_°C'] = time_aligned_data_avg['CVSDilAirDPTemp_Avg_°C']
-
-            # time_aligned_data['CVSFlow_mol'] = (
-            #         time_aligned_data_sum['CVSMolarFlow_Avg_mol/s'] * constants['SamplePeriod_s'])
-
             time_aligned_data_sum['BagFillFlow_Avg_m³/s'] = time_aligned_data_sum['BagFillFlow_Avg_l/min'] / 60000
 
             time_aligned_data['CVSFlow_mol'] = (
